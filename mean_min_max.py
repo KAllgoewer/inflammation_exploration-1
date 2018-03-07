ff0432400d4f0de699487ff1bac24ca4a640f540
grace hallenbeck <geh261@nyu.edu>
2018-03-07 10:28:23 -0500

    change order of plots

--- mean_min_max.py
+++ mean_min_max.py
@@ -1,3 +1,4 @@
+
 import numpy
 import matplotlib.pyplot
 
@@ -5,9 +6,9 @@ data = numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
 
 fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
 #make 3 subplots 
-axes1 = fig.add_subplot(1, 3, 1)
-axes2 = fig.add_subplot(1, 3, 2)
-axes3 = fig.add_subplot(1, 3, 3)
+axes1 = fig.add_subplot(1, 3, 3)
+axes2 = fig.add_subplot(1, 3, 1)
+axes3 = fig.add_subplot(1, 3, 2)
 
 axes1.set_ylabel('average')
 axes1.plot(numpy.mean(data, axis=0))
